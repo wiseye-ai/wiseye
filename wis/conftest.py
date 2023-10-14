ce348c25883bacb770a39168dd4f7aedef96045f
Mateusz Ernst <erniq1998@gmail.com>
2023-10-14 17:58:01 +0200

    Initial

---
+++ wis/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wis.users.models import User
+from wis.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
