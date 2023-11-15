01294fb5fbae1d345933066179ec62caf6fd31e3
jadwi2000 <jadwi.wrobel@gmail.com>
2023-11-15 22:36:40 +0100

    Added uuid to user model

--- wis/users/models.py
+++ wis/users/models.py
@@ -1,4 +1,7 @@
+import uuid as uuid
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField, EmailField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -19,6 +22,7 @@ class User(AbstractUser):
     last_name = None  # type: ignore
     email = EmailField(_("email address"), unique=True)
     username = None  # type: ignore
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
