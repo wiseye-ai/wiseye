fe47d91c092cb4f902651ac1cc54ad52d91fa6c6
Mateusz Ernst <erniq1998@gmail.com>
2023-11-22 21:46:21 +0100

    Added ImageField to User model

--- wis/users/models.py
+++ wis/users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db.models import CharField, EmailField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from filer.fields.image import FilerImageField
 
 from wis.users.managers import UserManager
 
@@ -23,6 +24,14 @@ class User(AbstractUser):
     email = EmailField(_("email address"), unique=True)
     username = None  # type: ignore
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, db_index=True)
+
+    image = FilerImageField(
+        verbose_name=_("image"),
+        null=True,
+        blank=True,
+        related_name="user_image",
+        on_delete=models.SET_NULL,
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
